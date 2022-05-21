afce8452314daaf8916d5c4939fe38a282b5420d
Jon Pepler <jonathan.pepler@gmail.com>
2022-05-21 22:54:46 +0100

    Test: show the session code when loaded
    Closes #2

--- src/components/CreateSession.test.js
+++ src/components/CreateSession.test.js
@@ -1,8 +1,23 @@
 import {render, screen} from '@testing-library/react'
 
 import {CreateSession} from './CreateSession'
+import {makePeer} from '../utils'
+
+jest.mock('../utils')
 
 it('shows a loading message when waiting for a session ID', async () => {
+  makePeer.mockImplementation(() => ({
+    on: () => {}
+  }))
   render(<CreateSession setRole={() => {}} setConnectionList={() => {}} connectionList={[]} />)
   expect(screen.getByText('Getting session code...')).toBeInTheDocument()
+})
+
+it('shows the session code when loaded', async () => {
+  const code = 'testcode'
+  makePeer.mockImplementation(() => ({
+    on: (_, setCode) => setCode(code)
+  }))
+  render(<CreateSession setRole={() => {}} setConnectionList={() => {}} connectionList={[]} />)
+  expect(screen.getByText(code)).toBeInTheDocument()
 })
