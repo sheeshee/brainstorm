ac16a6c9b2d4fb3bb2dde75ca9f13a47469cecaa
Sam Sheehy <samuelsheehy95@gmail.com>
2022-06-05 12:19:48 +0100

    Remove extra space

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 import './Button.css'
 
 export const Button = ({ text, onClick }) => {
-  return <button className="hot-button" onClick={onClick}>{text} </button>
+  return <button className="hot-button" onClick={onClick}>{text}</button>
 }
