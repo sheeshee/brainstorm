9f380532ebd8edeeff7f8ffb24ff505123b52747
Sam Sheehy <samuelsheehy95@gmail.com>
2023-01-10 17:47:57 +0000

    Replace server with public PeerJS one
    Heroku has started charging for apps, so we now revert to the free
    PeerJS one.

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,7 @@
 import Peer from 'peerjs'
 
 export const makePeer = () => {
-  const peer = new Peer({
-    secure: true,
-    host: 'rooster-thought.herokuapp.com', // change here the herokuapp name
-    port: 443
-  })
+  const peer = new Peer()
   peer.on('error', (err) => console.error(err))
   return peer
 }
